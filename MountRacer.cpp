cbe3b27c792f39bdf23e29c5961b037a4e428aab
SPolton <scott.polton@ucalgary.ca>
2026-01-13 20:31:48 -0700

    build: add initial CMake configuration

---
+++ MountRacer.cpp
@@ -0,0 +1,12 @@
+﻿// MountRacer.cpp : Defines the entry point for the application.
+//
+
+#include "MountRacer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
