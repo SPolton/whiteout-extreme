38216ac17eb771078ced85b0ce01c456b3c63512
SPolton <scott.polton@ucalgary.ca>
2026-01-24 12:34:54 -0700

    chore: create text rendering functions with FreeType

---
+++ src/core/Text.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "core/Shader.h"
+#include <glm.hpp>
+#include <map>
+
+struct Character {
+    unsigned int textureID; // ID handle of texture
+    glm::ivec2 size;        // Size of Glyph
+    glm::ivec2 bearing;     // Offset from baseline
+    unsigned int advance;   // Offset to next Glyph
+};
+
+void initTextVAO(unsigned int* VAO, unsigned int* VBO);
+
+std::map<char, Character> initFont(const char* font);
+
+void RenderText(Shader& s, unsigned int VAO, unsigned int VBO, std::string text,
+    float x, float y, float scale, glm::vec3 color, std::map<char, Character> Characters);
