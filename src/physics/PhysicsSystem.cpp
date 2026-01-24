1e9ec7f90577aee9e7e6bb3931d5a022cffa7c48
SPolton <scott.polton@ucalgary.ca>
2026-01-24 16:34:48 -0700

    chore: create physics system class with entity list and transforms

---
+++ src/physics/PhysicsSystem.cpp
@@ -0,0 +1,126 @@
+
+#include "PhysicsSystem.h"
+
+PhysicsSystem::PhysicsSystem() {
+    initPhysicsSystem();
+
+    // Reserve space for boxes in entity list
+    entityList.reserve(465);
+    initBoxes();
+}
+
+void PhysicsSystem::initPhysicsSystem() {
+    // Initialize PhysX
+    gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+    if (!gFoundation)
+    {
+        std::cout << "PxCreateFoundation failed!" << std::endl;
+        throw std::runtime_error("Failed to create PhysX foundation!");
+    }
+
+    // PVD
+    gPvd = PxCreatePvd(*gFoundation);
+    physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+    gPvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+
+    // Physics
+    gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, physx::PxTolerancesScale(), true, gPvd);
+    if (!gPhysics)
+    {
+        std::cout << "PxCreatePhysics failed!" << std::endl;
+        throw std::runtime_error("Failed to create PhysX physics!");
+    }
+
+    // Scene
+    physx::PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+    sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
+    gDispatcher = physx::PxDefaultCpuDispatcherCreate(2);
+    sceneDesc.cpuDispatcher = gDispatcher;
+    sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
+    gScene = gPhysics->createScene(sceneDesc);
+
+    // Prep PVD
+    physx::PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+    if (pvdClient)
+    {
+        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+    }
+
+    // Simulate
+    gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+    physx::PxRigidStatic* groundPlane = physx::PxCreatePlane(*gPhysics, physx::PxPlane(0, 1, 0, 50), *gMaterial);
+    gScene->addActor(*groundPlane);
+
+    std::cout << "Physics Test initialized successfully." << std::endl;
+}
+
+physx::PxVec3 PhysicsSystem::getPos(int i)
+{
+    // get position
+    physx::PxVec3 position = rigidDynamicList[i]->getGlobalPose().p;
+    return position;
+}
+
+void PhysicsSystem::updateTransforms() {
+
+    for (int i = 0; i < transformList.size(); i++) {
+
+        // store positions
+        transformList[i]->pos.x = rigidDynamicList[i]->getGlobalPose().p.x;
+        transformList[i]->pos.y = rigidDynamicList[i]->getGlobalPose().p.y;
+        transformList[i]->pos.z = rigidDynamicList[i]->getGlobalPose().p.z;
+
+        // store rotations
+        transformList[i]->rot.x = rigidDynamicList[i]->getGlobalPose().q.x;
+        transformList[i]->rot.y = rigidDynamicList[i]->getGlobalPose().q.y;
+        transformList[i]->rot.z = rigidDynamicList[i]->getGlobalPose().q.z;
+        transformList[i]->rot.w = rigidDynamicList[i]->getGlobalPose().q.w;
+    }
+}
+
+void PhysicsSystem::update(double delta_time) {
+    gScene->simulate(delta_time);
+    gScene->fetchResults(true);
+
+    updateTransforms();
+
+    physx::PxVec3 objPos = getPos(50);
+    std::cout << "x: " << objPos.x << " y: " << objPos.y << " z: " << objPos.z << std::endl;
+    std::cout << entityList[50].transform->pos.y << std::endl;
+}
+
+void PhysicsSystem::initBoxes()
+{
+    // Define a box
+    float halfLen = 0.5f;
+    physx::PxShape* shape = gPhysics->createShape(physx::PxBoxGeometry(halfLen, halfLen, halfLen), *gMaterial);
+    physx::PxU32 size = 30;
+    physx::PxTransform tran(physx::PxVec3(0));
+
+    // Create a pyramid of physics-enabled boxes
+    for (physx::PxU32 i = 0; i < size; i++)
+    {
+        for (physx::PxU32 j = 0; j < size - i; j++)
+        {
+            physx::PxTransform localTran(physx::PxVec3(physx::PxReal(j * 2) - physx::PxReal(size - i), physx::PxReal(i * 2 - 1), 0) * halfLen);
+            physx::PxRigidDynamic* body = gPhysics->createRigidDynamic(tran.transform(localTran));
+
+            // Store entity and transform pointers
+            rigidDynamicList.push_back(body);
+
+            body->attachShape(*shape);
+            physx::PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+            gScene->addActor(*body);
+        }
+    }
+
+    // Prepare transform list for updates
+    updateTransforms();
+
+    // Clean up
+    shape->release();
+
+    std::cout << "Box test spawned successfully." << std::endl;
+}
---
+++ src/physics/PhysicsSystem.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "PxPhysicsAPI.h"
+#include "components/Entity.h"
+#include "components/Transform.h"
+#include <vector>
+#include <iostream>
+
+class PhysicsSystem {
+public:
+    std::vector<Entity> entityList;
+
+    // Constructor
+    PhysicsSystem();
+
+    void update(double delta_time);
+    physx::PxVec3 getPos(int i);
+
+    std::vector<Transform*> transformList;
+    void updateTransforms();
+private:
+    std::vector<physx::PxRigidDynamic*> rigidDynamicList;
+
+    // PhysX management class instances.
+    physx::PxDefaultAllocator gAllocator;
+    physx::PxDefaultErrorCallback gErrorCallback;
+    physx::PxFoundation* gFoundation = NULL;
+    physx::PxPhysics* gPhysics = NULL;
+    physx::PxDefaultCpuDispatcher* gDispatcher = NULL;
+    physx::PxScene* gScene = NULL;
+    physx::PxMaterial* gMaterial = NULL;
+    physx::PxPvd* gPvd = NULL;
+
+    void initPhysicsSystem();
+    void initBoxes();
+};
