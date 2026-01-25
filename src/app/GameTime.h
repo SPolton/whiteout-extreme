21177412ca86c4b6c69073487dacf59989aa76a1
SPolton <scott.polton@ucalgary.ca>
2026-01-25 11:50:22 -0700

    refactor: move GameTime struct to own header

---
+++ src/app/GameTime.h
@@ -0,0 +1,17 @@
+
+#include <GLFW/glfw3.h>
+
+struct GameTime {
+    double t = 0.0;
+    const double dt = 1.0 / 60.0; // simulate at 60fps
+    double currentTime = glfwGetTime();
+    double accumulator = 0.0;
+
+    void update() {
+        // New Time Trackers
+        double newTime = glfwGetTime();
+        double frameTime = newTime - currentTime;
+        currentTime = newTime;
+        accumulator += frameTime;
+    }
+};
--- src/app/RacingGame.h
+++ src/app/RacingGame.h
@@ -1,27 +1,12 @@
 ﻿#pragma once
 
+#include "app/GameTime.h"
 #include "core/RenderingSystem.h"
 #include "core/Text.h"
 #include "physics/PhysicsSystem.h"
 
 #include <iostream>
 #include <memory>
-#include <GLFW/glfw3.h>
-
-struct GameTime {
-    double t = 0.0;
-    const double dt = 1.0 / 60.0; // simulate at 60fps
-    double currentTime = glfwGetTime();
-    double accumulator = 0.0;
-
-    void update() {
-        // New Time Trackers
-        double newTime = glfwGetTime();
-        double frameTime = newTime - currentTime;
-        currentTime = newTime;
-        accumulator += frameTime;
-    }
-};
 
 class RacingGame {
 public:
