96c2d5d4c6bed3703ce3d43e941cb65f950e220c
SPolton <scott.polton@ucalgary.ca>
2026-01-28 15:39:03 -0700

    build(cmake): add fmt.dev and vivid dependencies for logger

---
+++ cmake/text.cmake
@@ -0,0 +1,37 @@
+
+# fmt.dev
+message(STATUS "Using fmt.dev via FetchContent")
+
+set(FMT_TEST OFF CACHE BOOL "" FORCE)
+set(FMT_INSTALL OFF CACHE BOOL "" FORCE)
+
+FetchContent_Declare(
+    fmt
+    GIT_REPOSITORY https://github.com/fmtlib/fmt
+    GIT_TAG 11.0.2 # 12.1.0
+    GIT_SHALLOW TRUE
+)
+FetchContent_MakeAvailable(fmt)
+
+if(MSVC)
+    set_property(TARGET fmt PROPERTY MSVC_RUNTIME_LIBRARY ${MT_CONFIG})
+endif()
+
+target_link_libraries(${APP_NAME} PRIVATE fmt::fmt)
+
+
+# vivid
+message(STATUS "Using vivid via FetchContent")
+FetchContent_Declare(
+    vivid
+    GIT_REPOSITORY https://github.com/gurki/vivid
+    GIT_TAG v3.1.0
+    GIT_SHALLOW TRUE
+)
+FetchContent_MakeAvailable(vivid)
+
+if(MSVC)
+    set_property(TARGET vivid PROPERTY MSVC_RUNTIME_LIBRARY ${MT_CONFIG})
+endif()
+
+target_link_libraries(${APP_NAME} PRIVATE vivid)
