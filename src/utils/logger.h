37a8122157908ec0531ca7323a21f6d3b041ad12
SPolton <scott.polton@ucalgary.ca>
2026-01-28 17:20:42 -0700

    feat: log levels

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -30,6 +30,23 @@
 namespace logger {
     namespace ansi = vivid::ansi;
 
+    /// Logging levels
+    enum class Level {
+        TRACE,
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        FATAL,
+        OFF
+    };
+
+    static inline Level current_level = Level::DEBUG;
+
+    inline bool is_level_enabled(Level level) {
+        return level >= current_level;
+    }
+
     template <typename S1, typename S2, typename S3, typename... Args>
     void _log(const S1& prefix, const S2& c, const S3& format_str, Args&&... args) {
         fmt::print(
@@ -43,35 +60,50 @@ namespace logger {
 
     template <typename S, typename... Args>
     void trace(const S& format_str, Args&&... args) {
-        _log("TRACE", ansi::subtleText, format_str, args...);
+        if (is_level_enabled(Level::TRACE)) {
+            _log("TRACE", ansi::subtleText, format_str, args...);
+        }
     }
 
     template <typename S, typename... Args>
     void debug(const S& format_str, Args&&... args) {
-        _log("DEBUG", ansi::green, format_str, args...);
+        if (is_level_enabled(Level::DEBUG)) {
+            _log("DEBUG", ansi::blue, format_str, args...);
+        }
     }
 
     template <typename S, typename... Args>
     void info(const S& format_str, Args&&... args) {
-        _log("INFO", ansi::white, format_str, args...);
+        if (is_level_enabled(Level::INFO)) {
+            _log("INFO ", ansi::green, format_str, args...);
+        }
     }
 
     template <typename S, typename... Args>
     void warning(const S& format_str, Args&&... args) {
-        _log("WARN", ansi::yellow, format_str, args...);
+        if (is_level_enabled(Level::WARN)) {
+            _log("WARN ", ansi::yellow, format_str, args...);
+        }
     }
     template <typename S, typename... Args>
     void warn(const S& format_str, Args&&... args) {
-        _log("WARN", ansi::yellow, format_str, args...);
+        if (is_level_enabled(Level::WARN)) {
+            _log("WARN ", ansi::yellow, format_str, args...);
+        }
     }
 
     template <typename S, typename... Args>
     void error(const S& format_str, Args&&... args) {
-        _log("ERROR", ansi::red, format_str, args...);
+        if (is_level_enabled(Level::ERROR)) {
+            _log("ERROR", ansi::red, format_str, args...);
+        }
     }
+
     template <typename S, typename... Args>
     void fatal(const S& format_str, Args&&... args) {
-        _log("FATAL", ansi::lightRed, format_str, args...);
+        if (is_level_enabled(Level::FATAL)) {
+            _log("FATAL", ansi::lightRed, format_str, args...);
+        }
     }
 
     inline void test_logger_messages() {
