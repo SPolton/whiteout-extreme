f800ed53105985e48f8fe8364fcf354172ce97e8
SPolton <scott.polton@ucalgary.ca>
2026-01-31 15:44:23 -0700

    refactor: use physx namespace in cpp files only

--- src/physics/PhysicsSystem.cpp
+++ src/physics/PhysicsSystem.cpp
@@ -1,6 +1,9 @@
 #include "PhysicsSystem.hpp"
 #include "utils/logger.h"
 
+// OK in cpp files, not in headers
+using namespace physx;
+
 PhysicsSystem::PhysicsSystem() {
     initPhysicsSystem();
 
@@ -20,11 +23,11 @@ void PhysicsSystem::initPhysicsSystem() {
 
     // PVD
     gPvd = PxCreatePvd(*gFoundation);
-    physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
-    gPvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+    PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+    gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
 
     // Physics
-    gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, physx::PxTolerancesScale(), true, gPvd);
+    gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
     if (!gPhysics)
     {
         logger::error("PxCreatePhysics failed!");
@@ -32,34 +35,34 @@ void PhysicsSystem::initPhysicsSystem() {
     }
 
     // Scene
-    physx::PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
-    sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
-    gDispatcher = physx::PxDefaultCpuDispatcherCreate(2);
+    PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+    sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+    gDispatcher = PxDefaultCpuDispatcherCreate(2);
     sceneDesc.cpuDispatcher = gDispatcher;
-    sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
+    sceneDesc.filterShader = PxDefaultSimulationFilterShader;
     gScene = gPhysics->createScene(sceneDesc);
 
     // Prep PVD
-    physx::PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+    PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
     if (pvdClient)
     {
-        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
-        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
-        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
     }
 
     // Simulate
     gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
-    physx::PxRigidStatic* groundPlane = physx::PxCreatePlane(*gPhysics, physx::PxPlane(0, 1, 0, 50), *gMaterial);
+    PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0, 1, 0, 50), *gMaterial);
     gScene->addActor(*groundPlane);
 
     logger::info("Physics Test initialized successfully.");
 }
 
-physx::PxVec3 PhysicsSystem::getPos(int i)
+PxVec3 PhysicsSystem::getPos(int i)
 {
     // get position
-    physx::PxVec3 position = rigidDynamicList[i]->getGlobalPose().p;
+    PxVec3 position = rigidDynamicList[i]->getGlobalPose().p;
     return position;
 }
 
@@ -86,7 +89,7 @@ gScene->fetchResults(true);
 
     updateTransforms();
 
-    physx::PxVec3 objPos = getPos(50);
+    PxVec3 objPos = getPos(50);
     if (objPos.y < lastBoxPos.y) {
         logger::debug("x: {0} y: {1} z: {2}", objPos.x, objPos.y, objPos.z);
         logger::debug("Entity y: {0}", entityList[50].transform->pos.y);
@@ -98,17 +101,17 @@ void PhysicsSystem::initBoxes()
 {
     // Define a box
     float halfLen = 0.5f;
-    physx::PxShape* shape = gPhysics->createShape(physx::PxBoxGeometry(halfLen, halfLen, halfLen), *gMaterial);
-    physx::PxU32 size = 30;
-    physx::PxTransform tran(physx::PxVec3(0));
+    PxShape* shape = gPhysics->createShape(PxBoxGeometry(halfLen, halfLen, halfLen), *gMaterial);
+    PxU32 size = 30;
+    PxTransform tran(PxVec3(0));
 
     // Create a pyramid of physics-enabled boxes
-    for (physx::PxU32 i = 0; i < size; i++)
+    for (PxU32 i = 0; i < size; i++)
     {
-        for (physx::PxU32 j = 0; j < size - i; j++)
+        for (PxU32 j = 0; j < size - i; j++)
         {
-            physx::PxTransform localTran(physx::PxVec3(physx::PxReal(j * 2) - physx::PxReal(size - i), physx::PxReal(i * 2 - 1), 0) * halfLen);
-            physx::PxRigidDynamic* body = gPhysics->createRigidDynamic(tran.transform(localTran));
+            PxTransform localTran(PxVec3(PxReal(j * 2) - PxReal(size - i), PxReal(i * 2 - 1), 0) * halfLen);
+            PxRigidDynamic* body = gPhysics->createRigidDynamic(tran.transform(localTran));
 
             // Store rigid body
             rigidDynamicList.push_back(body);
@@ -125,7 +128,7 @@ void PhysicsSystem::initBoxes()
             entityList.push_back(entity);
 
             body->attachShape(*shape);
-            physx::PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+            PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
             gScene->addActor(*body);
         }
     }
--- src/physics/PhysicsTest.cpp
+++ src/physics/PhysicsTest.cpp
@@ -1,6 +1,9 @@
 #include "PhysicsTest.hpp"
 #include "utils/logger.h"
 
+// OK in cpp files, not in headers
+using namespace physx;
+
 PhysicsTest::PhysicsTest() {
     if (initPhysicsTest() != 0)
     {
@@ -19,11 +22,11 @@ int PhysicsTest::initPhysicsTest() {
 
     // PVD
     gPvd = PxCreatePvd(*gFoundation);
-    physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
-    gPvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+    PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+    gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
 
     // Physics
-    gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, physx::PxTolerancesScale(), true, gPvd);
+    gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
     if (!gPhysics)
     {
         logger::error("PxCreatePhysics failed!");
@@ -31,25 +34,25 @@ int PhysicsTest::initPhysicsTest() {
     }
 
     // Scene
-    physx::PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
-    sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
-    gDispatcher = physx::PxDefaultCpuDispatcherCreate(2);
+    PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+    sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+    gDispatcher = PxDefaultCpuDispatcherCreate(2);
     sceneDesc.cpuDispatcher = gDispatcher;
-    sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
+    sceneDesc.filterShader = PxDefaultSimulationFilterShader;
     gScene = gPhysics->createScene(sceneDesc);
 
     // Prep PVD
-    physx::PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+    PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
     if (pvdClient)
     {
-        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
-        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
-        pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+        pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
     }
 
     // Simulate
     gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
-    physx::PxRigidStatic* groundPlane = physx::PxCreatePlane(*gPhysics, physx::PxPlane(0, 1, 0, 50), *gMaterial);
+    PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0, 1, 0, 50), *gMaterial);
     gScene->addActor(*groundPlane);
 
     logger::info("Physics Test initialized successfully.");
@@ -60,19 +63,19 @@ void PhysicsTest::initBoxTest()
 {
     // Define a box
     float halfLen = 0.5f;
-    physx::PxShape* shape = gPhysics->createShape(physx::PxBoxGeometry(halfLen, halfLen, halfLen), *gMaterial);
-    physx::PxU32 size = 30;
-    physx::PxTransform tran(physx::PxVec3(0));
+    PxShape* shape = gPhysics->createShape(PxBoxGeometry(halfLen, halfLen, halfLen), *gMaterial);
+    PxU32 size = 30;
+    PxTransform tran(PxVec3(0));
 
     // Create a pyramid of physics-enabled boxes
-    for (physx::PxU32 i = 0; i < size; i++)
+    for (PxU32 i = 0; i < size; i++)
     {
-        for (physx::PxU32 j = 0; j < size - i; j++)
+        for (PxU32 j = 0; j < size - i; j++)
         {
-            physx::PxTransform localTran(physx::PxVec3(physx::PxReal(j * 2) - physx::PxReal(size - i), physx::PxReal(i * 2 - 1), 0) * halfLen);
-            physx::PxRigidDynamic* body = gPhysics->createRigidDynamic(tran.transform(localTran));
+            PxTransform localTran(PxVec3(PxReal(j * 2) - PxReal(size - i), PxReal(i * 2 - 1), 0) * halfLen);
+            PxRigidDynamic* body = gPhysics->createRigidDynamic(tran.transform(localTran));
             body->attachShape(*shape);
-            physx::PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+            PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
             gScene->addActor(*body);
         }
     }
