7acee1f4fa988eb457223be60381b1a6b87c7f0c
SPolton <scott.polton@ucalgary.ca>
2026-03-28 22:13:05 -0600

    chore: move snow emitter up because of avalanche tilt

--- src/app/RacingGame.cpp
+++ src/app/RacingGame.cpp
@@ -256,7 +256,7 @@ RacingGame::RacingGame()
         .spawnRate = 250.0f,
         .particleLifetimeSec = 1.2f,
         .particleSize = 5.0f,
-        .localOffset = glm::vec3(0.0f, 0.0f, -11.0f)
+        .localOffset = glm::vec3(0.0f, 3.0f, -12.0f)
     });
     logger::info("Avalanche entity created");
 
